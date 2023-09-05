f6ac9eb5543321d1ec191f46921a5a3359144ffc
waynetanjingheng <wtjh00@gmail.com>
2023-09-05 05:42:39 -0400

    chore: add checkout with TMoney button in cart modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
 import PATHS from './constants/paths'
 import Home from './pages/Home'
 import MyCart from './pages/MyCart';
@@ -7,12 +8,14 @@ import MyCart from './pages/MyCart';
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-        <BrowserRouter>
-            <Routes>
-                <Route path={PATHS.HOME} element={<Home />} />
-                <Route path={PATHS.CART} element={<MyCart />} />
-            </Routes>
-        </BrowserRouter>
+        <ModalsProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={PATHS.HOME} element={<Home />} />
+                    <Route path={PATHS.CART} element={<MyCart />} />
+                </Routes>
+            </BrowserRouter>
+        </ModalsProvider>
     </MantineProvider>
   );
 }
